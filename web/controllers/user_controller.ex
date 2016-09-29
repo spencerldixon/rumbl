8d660b26ad2d1aeb9a84994acf60ddfd67f6be0b
Spencer Dixon <spencerlloyddixon@gmail.com>
2016-09-29 16:54:19 +0100

    Added plug for user authentication

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,13 +1,47 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+  alias Rumbl.User
 
   def index(conn, _params) do
-    users = Repo.all(Rumbl.User)
-    render conn, "index.html", users: users
+    case authenticate(conn) do
+      %Plug.Conn{halted: true} = conn ->
+        conn
+      conn ->
+        users = Repo.all(Rumbl.User)
+        render conn, "index.html", users: users
+    end
   end
 
   def show(conn, %{"id" => id}) do
     user = Repo.get(Rumbl.User, id)
     render conn, "show.html", user: user
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.registration_changeset(%User{}, user_params)
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenticate(conn) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access this page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt
+    end
   end
 end
