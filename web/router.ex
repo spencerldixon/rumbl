51c51d6f3c4033c0722b109df278ab0910d13d6b
Spencer Dixon <spencerlloyddixon@gmail.com>
2016-10-03 22:29:14 +0100

    Added slugs and watch video controller

--- web/router.ex
+++ web/router.ex
@@ -19,6 +19,7 @@ defmodule Rumbl.Router do
     resources "/users", UserController, only: [:new, :index, :show, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
     get "/",          PageController, :index
+    get "/watch/:id", WatchController, :show
   end
 
   scope "/manage", Rumbl do
