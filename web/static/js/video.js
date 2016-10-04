62938312b300cb810f092afb280e5f835ba60036
Spencer Dixon <spencerlloyddixon@gmail.com>
2016-10-04 14:47:21 +0100

    Clickable annotations

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -22,8 +22,18 @@ let Video = {
 
     let vidChannel = socket.channel("videos:" + videoId)
 
+    msgContainer.addEventListener("click", e => { e.preventDefault()
+      let seconds = e.target.getAttribute("data-seek") ||
+        e.target.parentNode.getAttribute("data-seek")
+        if(!seconds){ return }
+
+      Player.seekTo(seconds)
+    })
+
     vidChannel.join()
       .receive("ok", resp => {
+        let ids = resp.annotations.map(ann => ann.id)
+        if(ids.length > 0){ vidChannel.params.last_seen_id = Math.max(...ids) }
         this.scheduleMessages(msgContainer, resp.annotations)
       })
       .receive("error", reason => console.log("failed to join", reason))
@@ -36,6 +46,7 @@ let Video = {
     })
 
     vidChannel.on("new_annotation", (resp) => {
+      vidChannel.params.last_seen_at = resp.id
       this.renderAnnotation(msgContainer, resp)
     })
   },
